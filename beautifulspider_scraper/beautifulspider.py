e67868e9fb9905345fc45c1d3b48b6109f49fe49
Nishant Aswani <41016622+niniack@users.noreply.github.com>
2019-01-28 14:35:19 +0400

    Update beautifulspider.py

--- beautifulspider_scraper/beautifulspider.py
+++ beautifulspider_scraper/beautifulspider.py
@@ -11,7 +11,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from spidersecrets import NYU_NetID, NYU_Pass
+from spider(dummy)secrets import NYU_NetID, NYU_Pass
 
 class BeautifulSpider: 
 	#netid and password can be changed from spidersecrets.py
